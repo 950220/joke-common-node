7d72952aa42187dc8472ac540507f80e7185dd4f
lws <wensheng@yougola.com>
2019-12-10 14:55:51 +0800

    'question'

--- dbBase/sql.js
+++ dbBase/sql.js
@@ -2,6 +2,10 @@ const sql = {
   // user
   loginSql: 'select * from user where username = ?',
   serachUserInfoSql: '',
-  tokenUpdateSql: 'update user set token=?,tokenExpiredDate=? where id=?'
+  tokenUpdateSql: 'update user set token=?,tokenExpiredDate=? where id=?',
+  getQuestionAnxietySql: 'select * from question_anxiety order by rand() limit 10',
+  getQuestionCharmSql: 'select * from question_charm order by rand() limit 10',
+  getQuestionPrivitySql: 'select * from question_privity order by rand() limit 10',
+  getQuestionSafeSql: 'select * from question_safe order by rand() limit 10'
 }
 module.exports = sql
---
+++ routes/test.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+const conn = require('../utils/dbUtils.js');
+const sql = require('../dbBase/sql.js');
+
+/* GET home page. */
+router.post('/getQuestion', function(req, res, next) {
+  let query = req.body
+  let sqlStr = ''
+  switch (query.type) {
+    case 'anxiety':
+      sqlStr = sql.getQuestionAnxietySql;
+      break;
+    case 'charm':
+      sqlStr = sql.getQuestionCharmSql;
+      break;
+    case 'privity':
+      sqlStr = sql.getQuestionPrivitySql;
+      break;
+    case 'safe':
+      sqlStr = sql.getQuestionSafeSql;
+      break;
+    default: 
+      return res.json({
+        resultCode: 5000,
+        errorDescription: 'type不能为空'
+      })
+  }
+  conn.query(sqlStr, (err, results) => {
+    if (err) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '数据获取失败'
+      })
+    }
+    if (results.length === 0) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '不存在此数据'
+      })
+    }
+    return res.json({
+      data: results,
+      resultCode: 200
+    })
+  })
+});
+
+module.exports = router;
