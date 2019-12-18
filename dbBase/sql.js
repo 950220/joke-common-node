9f351dd2892ab9e1acb79d4ea702b18d3f360df7
lws <wensheng@yougola.com>
2019-12-18 11:39:16 +0800

    'result'

--- dbBase/sql.js
+++ dbBase/sql.js
@@ -2,7 +2,7 @@ const sql = {
   // user
   loginSql: 'select * from user where username = ?',
   registerSql: 'insert into user (username, password) values (?,?)',
-  serachUserInfoSql: 'select * from user where id = ?',
+  serachUserInfoSql: 'select * from user where id = ? as a left join(select )',
   serachInfoByPassSql: 'select * from user where password = ?',
   changePasswordSql: 'update user set password=? where id = ?',
   tokenUpdateSql: 'update user set token=?,tokenExpiredDate=? where id = ?',
@@ -10,6 +10,10 @@ const sql = {
   getQuestionCharmSql: 'select * from question_charm order by rand() limit 10',
   getQuestionPrivitySql: 'select * from question_privity order by rand() limit 10',
   getQuestionSafeSql: 'select * from question_safe order by rand() limit 10',
-  getJokeSql: 'select * from joke_list order by rand() limit 10'
+  getJokeSql: 'select * from joke_list order by rand() limit 10',
+  insertResutlSql: 'insert into result (userId,score,resultStr,testType) values(?,?,?,?)',
+  getResultScoreSql: 'select score, testType from result where id=?',
+  getResultLabelSql: 'select * from labels where score=? and testType=? order by  rand() limit 2',
+  getResultIntroSql: 'select * from result_intro where score=? and testType=? order by  rand() limit 2'
 }
 module.exports = sql
--- routes/test.js
+++ routes/test.js
@@ -46,5 +46,100 @@ router.post('/getQuestion', function(req, res, next) {
   })
 })
 
+router.post('/submitTest', function(req, res, next) {
+  let query = req.body
+  let score = 0
+  const testType = query.testType
+  const id = query.userId
+  const results = query.results
+  results.forEach(item => {
+    switch(item.selectItem) {
+      case 'A':
+        score += 10;
+        break;
+      case 'B':
+        score += 9;
+        break;
+      case 'C':
+        score += 8;
+        break;
+      case 'D':
+        score += 7;
+        break;
+      case 'E':
+        score += 6;
+        break;
+    }
+  });
+  conn.query(sql.insertResutlSql, [id, score, JSON.stringify(results), testType], (err, results) => {
+    if (err) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '提交测试失败'
+      })
+    }
+    if (!results) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '提交测试失败'
+      })
+    }
+    return res.json({
+      resultCode: 200,
+      data: {
+        testId: results.insertId
+      }
+    })
+  })
+})
+
+router.post('/getResultById', function(req, res, next) {
+  let query = req.body
+  conn.query(sql.getResultScoreSql, query.testId, async (err, results) => {
+    if (err) {
+      return res.json({
+        resultCode: 5000,
+        errorDescription: '获取失败'
+      })
+    }
+    let score = 0
+    let testType = ''
+    let tagList = []
+    let resultList = []
+    results.forEach((item) => {
+      score = item.score
+      testType = item.testType
+    })
+    console.log(score, testType)
+    await conn.query(sql.getResultLabelSql, [score, testType], (err, results) => {
+      console.log(results)
+      if (err) {
+        return res.json({
+          resultCode: 5000,
+          errorDescription: '获取失败'
+        })
+      }
+      tagList = results
+    })
+    await conn.query(sql.getResultIntroSql, [score, testType], (err, results) => {
+      console.log(err)
+      if (err) {
+        return res.json({
+          resultCode: 5000,
+          errorDescription: '获取失败'
+        })
+      }
+      resultList = results
+      return res.json({
+        resultCode: 200,
+        data: {
+          tagList,
+          resultList
+        }
+      })
+    })
+  })
+})
+
 
 module.exports = router;
